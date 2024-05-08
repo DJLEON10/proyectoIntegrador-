1735e9802f7a64475fa03748eeb4f8a0fe00b671
DJLEON10 <djleonm@ufpso.edu.co>
2024-05-07 20:32:55 -0500

    primera version

---
+++ src/composable/data.js
@@ -0,0 +1,26 @@
+import {ref} from 'vue'
+
+export function useName() {
+    const name = ref("pepe")
+
+    return { name: name }
+}
+
+
+
+export function arrays(){
+   let objeto []  {  
+    "encuesta":' habitante de calle ',
+    "preguntas1" : 'location',
+    "preguntas2": 'type',
+    "totales1" : 'total1'
+    "encuestados" : [
+        name:"ana",
+        id: "2220",
+        barrio : "gustavo alayon"
+    ]
+
+
+    }
+}
+
---
+++ src/views/regEncuestado.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {ref} from 'vue'
+import { useName } from '@/composable/data'
+
+//const { name } = useName()
+
+//const a = ref(0)
+</script>
+
+
+
+<template>
+  <div>
+    <form @submit.prevent="addItem">
+      <input v-model="newItem.name" placeholder="nombre" class="text-red-950" />
+      <input v-model="newItem.lastName" class="text-red-950" placeholder="apellido" />
+      <input v-model="newItem.documento" class="text-red-950" placeholder="documento" />
+      <input v-model="newItem.barrio" class="text-red-950" placeholder="barrio" />
+      <button type="submit">Add</button>
+    </form>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item.name }} {{ item.lastName }} {{ item.documento }} {{ item.barrio }}
+        <a href="#">encuesta1</a>
+      </li>
+    </ul>
+  </div>
+</template>
+    soy una nueva vista {{ a }}
+    {{ name }}
